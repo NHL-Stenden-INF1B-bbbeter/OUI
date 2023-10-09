dd25c161a4475be71c5b69da34834f0513d1441d
GeisJeuring <geis.jeuring@student.nhlstenden.com>
2023-10-09 11:36:46 +0200

    stylesheet verplaatst in nieuwe map en de link in website.php naar website.css aangepast

--- website.php
+++ website.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/week5/OUI/website.css">
+    <link rel="stylesheet" href="css/website.css">
     <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
     <title>OUI Website</title>
 </head>
