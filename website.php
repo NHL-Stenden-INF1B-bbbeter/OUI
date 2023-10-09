436b41503b679b68864a544e07ba6de3f3160ccb
Daan Rijfers <130649310+xYouGetPownedx@users.noreply.github.com>
2023-10-09 11:02:03 +0200

    Add files via upload

---
+++ website.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/week5/OUI/website.css">
+    <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
+    <title>OUI Website</title>
+</head>
+<body>
+    <header>
+        <div class="header-container">
+            <div class="header-logo">
+                <a href="#home"><img class="logo" src="/week5/OUI/Logos/Logo-01.png" alt="OUI"></a>
+            </div>
+            <div class="header-links">
+                <ul>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#about us">About us</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <div class="home" id="home">
+        <div class="home-content">OUI</div>
+    </div>
+</body>
+</html>
