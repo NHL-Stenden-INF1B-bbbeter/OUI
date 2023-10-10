9e0bfeee6e69c575c62d75a58e8e6c26b26f5218
Peter Möller <peter.moller@student.nhlstenden.com>
2023-10-10 11:46:49 +0200

    openingstijden toegevoegd

--- contact.php
+++ contact.php
@@ -11,9 +11,22 @@
     <div class=contact-container>
         <div class=contact-header>Contact</div>
         <div class=contact-information>
-            <p><b>Our Location</b><br>
-            Raadshuisplein 28<br>
-            Emmen</p>
+            <p>
+                <b>Our Location:</b><br>
+                Raadshuisplein 28<br>
+                Emmen<br><br>
+                <b>Contact:</b><br>
+                Mail: oui@mail.com<br>
+                Phone: 050-226435<br><br><br><br><br>
+                <b>Opening times:</b><br>
+                Monday:&emsp;&emsp;&nbsp;12:00-18:00<br>
+                Tuesday:&emsp;&ensp;&nbsp; 12:00-18:00<br>
+                Wednesday:&ensp; 12:00-18:00<br>
+                Thursday: &emsp;&nbsp; 12:00-18:00<br>
+                Friday:&emsp;&emsp;&emsp; 12:00-18:00<br>
+                Saturday:&emsp;&ensp;&nbsp; 12:00-18:00<br>
+                Sunday: &emsp;&emsp; 12:00-18:00<br>
+            </p>
         </div>
     </div>
 </body>
