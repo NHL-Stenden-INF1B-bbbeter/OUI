bf4a0a851e480d6c652efcf75dacb83c3dc425b5
Peter Möller <peter.moller@student.nhlstenden.com>
2023-10-10 11:04:05 +0200

    content toegevoegd, titel en locatie

--- contact.php
+++ contact.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact</title>
-    <link rel="stylesheet" href="css/website.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <link href='https://fonts.googleapis.com/css?family=Caveat' rel='stylesheet'>
 </head>
 <body>
     <div class=contact-container>
-
+        <div class=contact-header>Contact</div>
+        <div class=contact-information>
+            <p><b>Our Location</b><br>
+            Raadshuisplein 28<br>
+            Emmen</p>
+        </div>
     </div>
 </body>
 </html>
