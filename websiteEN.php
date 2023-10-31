ef81307e913c06f433b0037ba47bbe97f7e4d4d8
daan rijfers <daanrijfers@ziggo.nl>
2023-10-31 11:28:28 +0100

    Ik heb in de Engelse versie de icoontjes achter de menu gedeelte gezet

--- websiteEN.php
+++ websiteEN.php
@@ -65,7 +65,7 @@
                     <li><a href="#menu">Menu</a></li>
                     <li><a href="#about-us">About us</a></li>
                     <li><a href="#contact">Contact</a></li>
-                    <li><a href="website.php">Nederlands</a></li>
+                    <li><a href="websiteEN.php">English</a></li>
                 </ul>
             </div>
         </div>
@@ -84,6 +84,7 @@
         <div class="Pain-Croissant">
             <p>Pain Croissant or Petit Pain</p>
             <div class="Pain-list">
+            <img class="img_croissant" src="photos/illustrations_light blue_croissant.png" alt="croissant">
                 <ul class="food-list">
                     <li>Naturel/ Plain</li>
                     <li>Butter</li>
@@ -116,6 +117,7 @@
             <div class="sweets-container">
                 <p>Sweets</p>
                 <div class="sweets-list">
+                <img class="img_merengue" src="photos/illustrations_light blue_merengue.png" alt="merengue">
                     <ul class="sweet-list">
                         <li>Macrons</li>
                         <li>Paris-Brest</li>
@@ -140,6 +142,7 @@
     <p class="title-drink">Drinks</p>
     <div class="coffee-tea"><p>Coffee & Tea</p>
         <div class="coffee-tea-list">
+        <img class="img_coffee" src="photos/illustrations_light blue_coffee.png" alt="coffee">
             <ul class="drinks-list">
                 <li>Cappucino</li>
                 <li>Latte</li>
@@ -164,6 +167,7 @@
         <div class="cool-drinks">
             <p>Cool Drinks</p>
             <div class="cool-drink-list">
+            <img class="img_tea" src="photos/illustrations_light blue_tea.png" alt="tea">
                 <ul class="cool-dink-list">
                     <li>Home-made lemonade</li>
                     <li>Jus D’orange</li>
@@ -193,12 +197,14 @@
         <div class="review1">
             <h1>PIET</h1>
             <h1>&starf; &starf; &starf; &starf; &starf;</h1>
-            <p>“I was in Emmen for the first time and ended up here. What fantastic coffee, and the staff are very friendly. Hence five stars!”</p>
+            <p>“Ik was voor het eerst in Emmen en kwam hier terecht. Wat een fantastische koffie, 
+                en het personeel is erg vriendelijk. Vandaar vijf sterren!”</p>
         </div>
         <div class="review2">
             <h1>KLAAS</h1>
             <h1>&starf; &starf; &starf; &starf; &starf;</h1>
-            <p>“I study in Emmen and wanted a cup of coffee. When I came to OUI it felt absolutely right. The coffee is tasty and the staff is friendly. Five stars!”</p>
+            <p>“Ik studeer in Emmen, en had zin in een kopje koffie. Toen ik bij OUI kwam voelde het helemaal goed. 
+                De koffie is lekker en het personeel is vriendelijk. Vijf sterren!”</p>
         </div>
     </div>
     <div id="about-us"></div>
@@ -208,12 +214,14 @@
         </div>
         <div class="about-us">
         <p>
-            Welcome to OUI! We greet every customer with open arms and a hot cup of coffee. Our mission is simple but of great importance: we want to make sure that you never stand in front of a closed door and that you find your way to us effortlessly. At OUI we strive to make your experience unforgettable. Our coffee is made with love, and our team is always ready to welcome you in a hospitable and cozy environment.
+            Welkom bij OUI! Wij begroeten elke klant met open armen en een warme kop koffie. Onze missie is simpel maar van groot belang: we willen er zeker van zijn dat u nooit voor een gesloten deur staat en dat u moeiteloos de weg naar ons vindt. Bij OUI streven we ernaar om uw ervaring onvergetelijk te maken. Onze koffie is met liefde gezet, en ons team staat altijd klaar om u te verwelkomen in een gastvrije en gezellige omgeving.
         </p><br>
         <p>
-            Whether you're just stopping by to enjoy a quiet moment with a book, joining us for breakfast with friends, or planning a business meeting, we're always here to help. We offer an extensive menu with delicious dishes and refreshing drinks, so there is something for everyone.</p><br>
+            Of u nu alleen langskomt om te genieten van een rustig moment met een boek, met vrienden bij ons komt ontbijten, of een zakelijke bijeenkomst plant, we staan altijd voor u klaar. We bieden een uitgebreid menu met heerlijke gerechten en verfrissende drankjes, zodat er voor ieder wat wils is.
+        </p><br>
         <p>
-            Thanks to our central location in the heart of the city, you can easily reach us and we are an ideal meeting place for everyone. We look forward to welcoming you to OUI and making your day a little more special.</p><br>
+            Dankzij onze centrale locatie in het hart van de stad, kunt u ons gemakkelijk bereiken en zijn we een ideale ontmoetingsplek voor iedereen. We kijken ernaar uit om u te verwelkomen bij OUI en uw dag een beetje specialer te maken.
+        </p><br>
         </div>
     </div>
     <div id="container-slider">
@@ -246,12 +254,12 @@
                 Phone: 050-226435<br><br><br><br><br>
                 <b>Opening times:</b><br>
                 Monday:&emsp;&emsp;&nbsp;12:00-18:00<br>
-                Tuesday:&emsp;&ensp;&nbsp; 12:00-18:00<br>
+                Tuesday:&emsp;&ensp;&nbsp;&nbsp; 12:00-18:00<br>
                 Wednesday:&ensp; 12:00-18:00<br>
                 Thursday: &emsp;&nbsp; 12:00-18:00<br>
                 Friday:&emsp;&emsp;&emsp; 12:00-18:00<br>
                 Saturday:&emsp;&ensp;&nbsp; 12:00-18:00<br>
-                Sunday: &emsp;&emsp; 12:00-18:00<br>
+                Sunday: &emsp;&emsp;&nbsp; 12:00-18:00<br>
             </p>
         </div>
         <div id="myMap" class="maps-area"></div>
